5793a47a5a52255829e45ba749dd81a06fe98370
Matt Heard <matt@mattheard.net>
2015-02-16 20:42:38 +1300

    Tested the unit test framework.

--- practical/201/test_double_priority_queue.py
+++ practical/201/test_double_priority_queue.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
-def runUnitTests():
-        print("Hello, unit tests!")
+import unittest
+
+class TestDoublePriorityQueue(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtract(self):
+        self.assertEqual(2 - 1, 1)
+
+    def test_multiply(self):
+        self.assertEqual(2 * 2, 4)
+
+    def test_divide(self):
+        self.assertEqual(4 / 2, 2)
 
 if __name__ == "__main__":
-        runUnitTests()
+    unittest.main()
