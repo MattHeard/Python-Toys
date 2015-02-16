d37b48ae90e8baddefa5a2187aec3f726df53167
Matt Heard <matt@mattheard.net>
2015-02-16 21:44:37 +1300

    Added one node to DPQ.

--- practical/201/double_priority_queue.py
+++ practical/201/double_priority_queue.py
@@ -1,5 +1,8 @@
 class DoublePriorityQueue:
 
+    def __init__(self):
+        self.count = 0
+
     def Enqueue(self, val, priorityA, priorityB):
         node = Node(val, priorityA, priorityB)
         self.push(node)
@@ -11,13 +14,17 @@ class DoublePriorityQueue:
         return self.popPriorityB()
 
     def Count(self):
-        return 0
+        return self.count
 
     def Clear(self):
         pass
 
     def push(self, node):
-        pass
+        if self.Count() is 0:
+            self.first = node
+            self.topPriorityA = node
+            self.topPriorityB = node
+            self.count += 1
 
     def popPriorityA(self):
         pass
