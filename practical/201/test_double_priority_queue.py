341b5438c4abd21d3da6d5ebb8a690daa570176b
Matt Heard <matt@mattheard.net>
2015-02-16 21:19:04 +1300

    Unit tested length of empty DPQ.

--- practical/201/test_double_priority_queue.py
+++ practical/201/test_double_priority_queue.py
@@ -2,19 +2,15 @@
 
 import unittest
 
+from double_priority_queue import DoublePriorityQueue
+
 class TestDoublePriorityQueue(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(1 + 1, 2)
-
-    def test_subtract(self):
-        self.assertEqual(2 - 1, 1)
-
-    def test_multiply(self):
-        self.assertEqual(2 * 2, 4)
-
-    def test_divide(self):
-        self.assertEqual(4 / 2, 2)
+    def test_countEmptyDpq(self):
+        dpq = DoublePriorityQueue()
+        expected = 0
+        actual = dpq.Count()
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
