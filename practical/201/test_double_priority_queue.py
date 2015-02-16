624e4a37c3c8601f61d5d6016366c375482995c6
Matt Heard <matt@mattheard.net>
2015-02-16 21:22:33 +1300

    Added failing unit test for counting single node.

--- practical/201/test_double_priority_queue.py
+++ practical/201/test_double_priority_queue.py
@@ -6,11 +6,21 @@ from double_priority_queue import DoublePriorityQueue
 
 class TestDoublePriorityQueue(unittest.TestCase):
 
-    def test_countEmptyDpq(self):
+    def test_countEmpty(self):
         dpq = DoublePriorityQueue()
         expected = 0
         actual = dpq.Count()
         self.assertEqual(expected, actual)
 
+    def test_countSingleNode(self):
+        dpq = DoublePriorityQueue()
+        string = "Alice"
+        priorityA = 1.23
+        priorityB = 4.56
+        dpq.Enqueue(string, priorityA, priorityB)
+        expected = 1
+        actual = dpq.Count()
+        self.assertEqual(expected, actual)
+    
 if __name__ == "__main__":
     unittest.main()
